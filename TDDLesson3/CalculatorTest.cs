81885377912ba668817d7b3e906761b186bc16d4
Andrew Wagner <andrew9010.av@gmail.com>
2021-11-14 03:06:11 +0600

    add NUnit Test

---
+++ TDDLesson3/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Calc = Calculator.Calculator;
+
+namespace TDDLesson3
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public static void TestOpertaions()
+        {
+            Calc calculator = new Calc();
+            Assert.AreEqual(calculator.Add(1, 3), 4);
+            Assert.AreEqual(calculator.Minus(1, 3), -2);
+            Assert.AreEqual(calculator.Muliply(1, 3), 3);
+            Assert.AreEqual(calculator.Divide(3, 1), 3);
+        }
+    }
+}
