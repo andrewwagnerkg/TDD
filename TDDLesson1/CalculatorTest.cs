4e6aedac944eb9dc521cc0c4a0b407626adadcd2
Andrew Wagner <andrew9010.av@gmail.com>
2021-11-14 02:20:18 +0600

    imp

--- TDDLesson1/CalculatorTest.cs
+++ TDDLesson1/CalculatorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace TDDLesson1
 {
@@ -13,17 +14,17 @@ namespace TDDLesson1
         public void TestOpertaions()
         {
             Calculator calc = new Calculator();
-            if (3 == calc.Add(1, 2)) _showMessage?.Invoke(passMessage);
-            else _showMessage?.Invoke(failedMessage);
+            if (3 == calc.Add(1, 2)) _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {passMessage}");
+            else _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {failedMessage}");
 
-            if (-1 == calc.Minus(1, 2)) _showMessage?.Invoke(passMessage);
-            else _showMessage?.Invoke(failedMessage);
+            if (-1 == calc.Minus(1, 2)) _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {passMessage}");
+            else _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {failedMessage}");
 
-            if (2 == calc.Divide(4, 2)) _showMessage?.Invoke(passMessage);
-            else _showMessage?.Invoke(failedMessage);
+            if (2 == calc.Divide(4, 2)) _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {passMessage}");
+            else _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {failedMessage}");
 
-            if (2 == calc.Muliply(1, 2)) _showMessage?.Invoke(passMessage);
-            else _showMessage?.Invoke(failedMessage);
+            if (2 == calc.Muliply(1, 2)) _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {passMessage}");
+            else _showMessage?.Invoke($"{MethodBase.GetCurrentMethod()?.Name} - {failedMessage}");
         }
     }
 }
