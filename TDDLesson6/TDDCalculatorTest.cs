6c4565e5fd096c9cf694f601b2fe40aa2771a892
Andrew Wagner <andrew9010.av@gmail.com>
2021-11-15 22:20:24 +0600

    TDD example

---
+++ TDDLesson6/TDDCalculatorTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using Calc= Calculator.Calculator;
+
+namespace TDDLesson6
+{
+    [TestFixture]
+    public class TDDCalculatorTest
+    {
+        private Calc _calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            _calculator = new Calc();
+        }
+
+        //[Test]
+        //public void PowTest()
+        //{
+        //    ///
+        //    /// Написать метод который будет возводить число в указаную степень
+        //    ///
+            
+        //}
+
+        //[Test]
+        //public void DivideByZeroTest()
+        //{
+        //    Assert.Throws<ArgumentException>(() => _calculator.Divide(24, 0));
+        //}
+
+        //[Test]
+        //public void SqrtTest()
+        //{
+
+        //}
+
+        //[Test]
+        //public void SqrtExceptionTest()
+        //{
+            
+        //}
+
+        [TearDown]
+        public void AfterTest()
+        {
+            _calculator = null;
+        }
+    }
+}
